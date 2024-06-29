73f3558c22fcb346d579b96246a531b6d3c7fea8
Mohammadshadain <shadain044714@gmail.com>
2024-06-29 17:23:14 +0530

    today i learned the basic of javaScript

---
+++ condition.js
@@ -0,0 +1,14 @@
+n=340;
+if (n%2==0) {
+    console.log('even');
+    something='hello';
+    let x='nice'
+    const pi=3.1416;
+    pi=3.555;
+} 
+else{
+    console.log('odd');
+}
+console.log(something);
+// console.log(pi);
+// console.log(x);
---
+++ index.js
@@ -0,0 +1,21 @@
+console.log('hello world');
+console.log('hello worlkjsdhklklhjdxkjd');
+
+a=980;
+console.log(a);
+console.log(typeof a);
+myname='shadain';
+console.log(myname);
+console.log(typeof myname);
+
+isLogin=true;
+console.log(isLogin)
+console.log(typeof isLogin);
+// exponent operator
+console.log(2**4);
+console.log(1==='1');
+console.log(1=='1');
+
+console.log(24+ 24+'45');
+console.log(24+ '45' +30);
+console.log(null+'45');
